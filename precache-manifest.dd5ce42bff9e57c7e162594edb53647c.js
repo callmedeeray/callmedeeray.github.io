072b41a24dbab545d5f5f4ae96dff95cc6ab8eee
msdisarray <desiree@Desirees-MacBook-Pro.local>
2019-09-21 23:42:35 -0700

    Updates

---
+++ precache-manifest.dd5ce42bff9e57c7e162594edb53647c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b404f3c7558527bda7289e2a5eb4077",
+    "url": "/lsst-project-dev/index.html"
+  },
+  {
+    "revision": "57af66817ef78ea67c55",
+    "url": "/lsst-project-dev/static/css/main.b185d6d3.chunk.css"
+  },
+  {
+    "revision": "56d90394d42b9c6c8a81",
+    "url": "/lsst-project-dev/static/js/2.0a6c041f.chunk.js"
+  },
+  {
+    "revision": "57af66817ef78ea67c55",
+    "url": "/lsst-project-dev/static/js/main.a91419b5.chunk.js"
+  },
+  {
+    "revision": "d83d0f54076dff35bddb",
+    "url": "/lsst-project-dev/static/js/runtime~main.427bd293.js"
+  },
+  {
+    "revision": "df425ff11e28aa9570d90359dba687c5",
+    "url": "/lsst-project-dev/static/media/logo.df425ff1.svg"
+  }
+]);
