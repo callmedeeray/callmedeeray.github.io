eed60416b6ccf54fb37ec880bbdcf651d84773ee
msdisarray <desiree@Desirees-MacBook-Pro.local>
2020-02-26 16:09:48 -0800

    Ok now it's rendering and we can really get to work

--- makeovermonday-2020-week08/src/index.js
+++ makeovermonday-2020-week08/src/index.js
@@ -6,33 +6,46 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 main();
+// window.addEventListener('resize', main());
+
+window.onresize = main;
+//Call our resize function if the window size is changed.
 
 function main() {
+
+	d3.select('#main').selectAll('g').remove();
 
 	let 
 		winWidth = Math.floor(0.975*window.innerWidth),
 		winHeight = Math.floor(0.975*window.innerHeight),
 		margin = {
-			top: Math.floor(winHeight*0.1),
-			bottom: Math.floor(winHeight*0.1),
-			left: Math.floor(winWidth*0.1),
-			right: Math.floor(winWidth*0.1)
-		},
-		width = winWidth - margin.left - margin.right, 
+			top: Math.floor(winHeight*0.05),
+			bottom: Math.floor(winHeight*0.05),
+			left: Math.floor(winWidth*0.05),
+			right: Math.floor(winWidth*0.05)
+		};
+
+	let width = document.getElementById('root').clientWidth - margin.left - margin.right,
 		height = winHeight - margin.top - margin.bottom;
 
+	d3.select('#main')
+		.attr('width', width)
+		.attr('height', height);
 
 	d3.select('body')
 		.style('margin-top', margin.top + 'px')
 		.style('margin-left', margin.left + 'px')
 		.style('margin-right', margin.right + 'px')
-		.style('margin-bottom', margin.bottom + 'px')
+		.style('margin-bottom', margin.bottom + 'px');
+
+	d3.select('#root')
+		.style('height', height + 'px');
 
 	let svg = d3.select('#main');
 
 	let nodes1 = [
-			{'node': 'Homeless0', 'name': 'Homeless0'}
-			, {'node': 'At risk0', 'name': 'At risk0'}
+			{'node': 'Homeless0', 'name': 'Homeless'}
+			, {'node': 'At risk0', 'name': 'At risk'}
 			, {'node': 'Homeless', 'name': 'Homeless'}
 			, {'node': 'At risk', 'name': 'At risk'}
 			, {'node': 'Unknown', 'name': 'Unknown'}
@@ -43,25 +56,11 @@ function main() {
 			// .nodeAlign(d3[`sankey${align[0].toUpperCase()}${align.slice(1)}`])
 			.nodeWidth(15)
 			.nodePadding(10)
-			.extent([[1, 1], [width - 1, height - 25]])({
+			.extent([[1, 1], [0.95*width, 0.95*height]])({
 				nodes: nodes1.map(d => Object.assign({}, d)),
 				links: data.filter(d => d.Year == '2014-15' ).map(d => Object.assign({}, d))
 			});
 
-	
-	console.log(links);
-
-
-	// let { nodes, links } = sankey()
-	// 	.nodeWidth(winWidth*0.05)
-	// 	.nodePadding(winWidth*0.01)
-	// 	.nodeId((d) => { d.node })
-	// 	.extent([[1, 1], [width - 1, height - 25]])
-	// 	({
-	// 					nodes: nodes1.map(d => Object.assign({}, d)),
-	// 					links: data.filter(d => d.Year == '2014-15' ).map(d => Object.assign({}, d))
-	// 			})
-	// 	;
 
 	let node = svg.append('g')
 		.selectAll('.node')
@@ -69,8 +68,13 @@ function main() {
 		.enter()
 		.append('rect')
 		.attr('class', 'node')
-		.attr('height', (d) => { d.y1 - d.y0 })
-		.attr('width', 5)
+		.attr('x', d => d.x0 )
+		.attr('y', d => d.y0 )
+		.attr('height', d => d.y1 - d.y0 )
+		.attr('width', d => d.x1 - d.x0 )
+		.attr('fill', 'blue')
+		.style('stroke', 'black')
+		.style('stroke-width', 1)
 		;
 
 	let link = svg.append('g')
@@ -80,8 +84,11 @@ function main() {
 		.append('path')
 		.attr('class', 'link')
 		.attr('d', sankeyLinkHorizontal())
-		.style('stroke-width', (d) => { d.width; })
-		.sort((a,b) => { b.dy - a.dy });
+		.attr('fill', 'none')
+		.style('stroke', 'grey')
+		.style('stroke-width', d => d.width )
+		.style('stroke-opacity', 0.5)
+		.sort((a,b) => b.dy - a.dy );
 
 
 }
