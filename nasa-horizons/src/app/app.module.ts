fd42e1a8c5a212f787e8ac6d785d1ed5e1c70047
Desiree' Abbott <callmedeeray@gmail.com>
2022-06-21 10:31:57 -0700

    Data displays on the page!

--- nasa-horizons/src/app/app.module.ts
+++ nasa-horizons/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';  
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocationsComponent } from './locations/locations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ nasa-horizons/src/app/locations/locations.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+// const DateTime = require('luxon');
+import { DateTime } from 'luxon'; 
+
+import { LocationRaw, Location } from '../location';
+
+
+@Component({
+  selector: 'app-locations',
+  templateUrl: './locations.component.html',
+  styleUrls: ['./locations.component.scss']
+})
+
+
+export class LocationsComponent implements OnInit {
+
+  locationsRaw: LocationRaw[] = [];
+  locations: Location[] = [];
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.httpClient
+      .get('assets/horizons_results.json')
+      .subscribe(data => {
+        this.locationsRaw = Object.values(data);
+        this.convertData();
+        console.log(this.locationsRaw);
+      });
+  }
+
+  convertData(): void {
+    this.locationsRaw.forEach(d => {
+      this.locations.push({
+        id: Math.floor(Number(d.jdtdb)),
+        calendar_date: (DateTime.fromFormat(d.calendar_date.split(' ')[1], 'yyyy-LLL-dd').toLocaleString(DateTime.DATE_MED)),
+        x: Number(d.x),
+        y: Number(d.y),
+        z: Number(d.z)
+      })
+    })
+  }
+
+}
